f1443565487300acff4028a13c711e886b37db43
singonwan <singonwan@gmail.com>
2024-09-25 16:35:47 -0400

    modified created profile page, fetch user profile and update profile page completed

--- utils/actions.ts
+++ utils/actions.ts
@@ -6,6 +6,24 @@ import { auth, clerkClient, currentUser } from '@clerk/nextjs/server';
 import { revalidatePath } from 'next/cache';
 import { redirect } from 'next/navigation';
 
+const getAuthUser = async () => {
+	const user = await currentUser();
+	if (!user) {
+		throw new Error('You must be logged in to access this route');
+	}
+	if (!user.privateMetadata.hasProfile) redirect('/profile/create');
+
+	// all good
+	return user;
+};
+
+const renderError = (error: unknown): { message: string } => {
+	console.log(error);
+	return {
+		message: error instanceof Error ? error.message : 'An error occurred',
+	};
+};
+
 export const createProfileAction = async (
 	prevState: any,
 	formData: FormData
@@ -33,9 +51,7 @@ export const createProfileAction = async (
 		});
 	} catch (error) {
 		// console.log(error);
-		return {
-			message: error instanceof Error ? error.message : 'An error occurred',
-		};
+		return renderError(error);
 	}
 	redirect('/');
 };
@@ -54,4 +70,40 @@ export const fetchProfileImage = async () => {
 	});
 
 	return profile?.profileImage;
+};
+
+export const fetchProfile = async () => {
+	const user = await getAuthUser();
+
+	const profile = await db.profile.findUnique({
+		where: {
+			clerkId: user.id,
+		},
+	});
+	if (!profile) redirect('/profile/create');
+	return profile;
+};
+
+export const updateProfileAction = async (
+	prevState: any,
+	formData: FormData
+): Promise<{ message: string }> => {
+	const user = await getAuthUser();
+
+	try {
+		const rawData = Object.fromEntries(formData);
+		const validatedFields = profileSchema.parse(rawData);
+
+		await db.profile.update({
+			where: {
+				clerkId: user.id,
+			},
+			data: validatedFields,
+		});
+
+		revalidatePath('/profile');
+		return { message: 'Profile updated successfully' };
+	} catch (error) {
+		return renderError(error);
+	}
 };
