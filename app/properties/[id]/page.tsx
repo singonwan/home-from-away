2664bc889328698d6a82ec2eefbd94b4b8868800
singonwan <singonwan@gmail.com>
2024-10-09 14:54:52 -0400

    created share button for property detail

--- app/properties/[id]/page.tsx
+++ app/properties/[id]/page.tsx
@@ -1,5 +1,6 @@
 import FavoriteToggleButton from '@/components/card/FavoriteToggleButton';
 import BreadCrumbs from '@/components/properties/BreadCrumbs';
+import ShareButton from '@/components/properties/ShareButton';
 import { fetchPropertyDetails } from '@/utils/actions';
 import { redirect } from 'next/navigation';
 
@@ -16,7 +17,7 @@ async function PropertyDetailsPage({ params }: { params: { id: string } }) {
 			<header className="flex justify-between items-center mt-4">
 				<h1 className="text-4xl font-bold capitalize">{property.tagline}</h1>
 				<div className="flex items-center gap-x-4">
-					{/* share button */}
+					<ShareButton name={property.name} propertyId={property.id} />
 					<FavoriteToggleButton propertyId={property.id} />
 				</div>
 			</header>
