c32deadab96ea699c88e211de70a476e127b6140
singonwan <singonwan@gmail.com>
2024-10-10 16:41:43 -0400

    adding map and amenities component to property details page

--- app/properties/[id]/page.tsx
+++ app/properties/[id]/page.tsx
@@ -1,5 +1,6 @@
 import FavoriteToggleButton from '@/components/card/FavoriteToggleButton';
 import PropertyRating from '@/components/card/PropertyRating';
+import Amenities from '@/components/properties/Amenities';
 import BookingCalendar from '@/components/properties/BookingCalendar';
 import BreadCrumbs from '@/components/properties/BreadCrumbs';
 import Description from '@/components/properties/Description';
@@ -8,8 +9,18 @@ import PropertyDetails from '@/components/properties/PropertyDetails';
 import ShareButton from '@/components/properties/ShareButton';
 import UserInfo from '@/components/properties/UserInfo';
 import { Separator } from '@/components/ui/separator';
+import { Skeleton } from '@/components/ui/skeleton';
 import { fetchPropertyDetails } from '@/utils/actions';
+import dynamic from 'next/dynamic';
 import { redirect } from 'next/navigation';
+
+const DynamicMap = dynamic(
+	() => import('@/components/properties/PropertyMap'),
+	{
+		ssr: false,
+		loading: () => <Skeleton className="h-[400px] w-full" />,
+	}
+);
 
 async function PropertyDetailsPage({ params }: { params: { id: string } }) {
 	const property = await fetchPropertyDetails(params.id);
@@ -41,6 +52,8 @@ async function PropertyDetailsPage({ params }: { params: { id: string } }) {
 					<UserInfo profile={{ firstName, profileImage }} />
 					<Separator className="mt-4" />
 					<Description description={property.description} />
+					<Amenities amenities={property.amenities} />
+					<DynamicMap countryCode={property.country} />
 				</div>
 				<div className="lg:col-span-4 flex flex-col items-center">
 					{/* calendar */}
