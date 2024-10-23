4885114bf0c3623da97f70cd953fcb4bd5aede55
singonwan <singonwan@gmail.com>
2024-10-23 14:47:12 -0400

    completed property rating component with real ratings

--- utils/actions.ts
+++ utils/actions.ts
@@ -366,3 +366,22 @@ export const deleteReviewAction = async (prevState: { reviewId: string }) => {
 		return renderError(error);
 	}
 };
+
+export async function fetchPropertyRating(propertyId: string) {
+	const result = await db.review.groupBy({
+		by: ['propertyId'],
+		_avg: {
+			rating: true,
+		},
+		_count: {
+			rating: true,
+		},
+		where: {
+			propertyId,
+		},
+	});
+	return {
+		rating: result[0]?._avg.rating?.toFixed() ?? 0,
+		count: result[0]?._count.rating ?? 0,
+	};
+}
