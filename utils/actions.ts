5657d669c0c259142315d54781600ff8822c65ab
singonwan <singonwan@gmail.com>
2024-10-23 18:25:31 -0400

    added types and components for booking

--- utils/actions.ts
+++ utils/actions.ts
@@ -277,6 +277,12 @@ export const fetchPropertyDetails = async (id: string) => {
 		},
 		include: {
 			profile: true,
+			bookings: {
+				select: {
+					checkIn: true,
+					checkOut: true,
+				},
+			},
 		},
 	});
 };
