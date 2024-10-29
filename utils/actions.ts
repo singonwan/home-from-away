4efdc12c313752f3bc19479bd0bd065b5aaa2b11
singonwan <singonwan@gmail.com>
2024-10-29 18:51:26 -0400

    set up rental edit page

--- utils/actions.ts
+++ utils/actions.ts
@@ -541,3 +541,21 @@ export const deleteRentalAction = async (prevState: { propertyId: string }) => {
 		return renderError(error);
 	}
 };
+
+export const fetchRentalDetails = async (propertyId: string) => {
+	const user = await getAuthUser();
+	return db.property.findUnique({
+		where: {
+			id: propertyId,
+			profileId: user.id,
+		},
+	});
+};
+
+export const updatePropertyAction = async () => {
+	return { message: 'update property action' };
+};
+
+export const updatePropertyImageAction = async () => {
+	return { message: 'update property image action' };
+};
