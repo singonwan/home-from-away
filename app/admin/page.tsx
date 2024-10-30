bb1c612b8a5c399e91510748a0b98c5cb4ad7ae1
singonwan <singonwan@gmail.com>
2024-10-30 14:58:39 -0400

    loading components for admin and stats card done

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,7 +1,22 @@
-import React from 'react';
+import ChartsContainter from '@/components/admin/ChartsContainter';
+import {
+	ChartsLoadingContainer,
+	StatsLoadingContainer,
+} from '@/components/admin/Loading';
+import StatsContainter from '@/components/admin/StatsContainter';
+import { Suspense } from 'react';
 
 function AdminPage() {
-	return <div>AdminPage</div>;
+	return (
+		<>
+			<Suspense fallback={<StatsLoadingContainer />}>
+				<StatsContainter />
+			</Suspense>
+			<Suspense fallback={<ChartsLoadingContainer />}>
+				<ChartsContainter />
+			</Suspense>
+		</>
+	);
 }
 
 export default AdminPage;
--- components/admin/StatsContainter.tsx
+++ components/admin/StatsContainter.tsx
@@ -1,7 +1,16 @@
-import React from 'react';
+import { fetchStats } from '@/utils/actions';
+import StatsCard from './StatsCard';
 
-function StatsContainter() {
-	return <div>StatsContainter</div>;
+async function StatsContainter() {
+	const data = await fetchStats();
+
+	return (
+		<div className="mt-8 grid md:grid-cols-2 gap-4 lg:grid-cols-3">
+			<StatsCard title="users" value={data.usersCount || 0} />
+			<StatsCard title="properties" value={data.propertiesCount || 0} />
+			<StatsCard title="bookings" value={data.bookingsCount || 0} />
+		</div>
+	);
 }
 
 export default StatsContainter;
